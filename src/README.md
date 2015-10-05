f1f694d847cb65fad72f294d917b88da6dcf578b
Jonathan Le Greneur <jonathan.legreneur@free.fr>
2015-10-05 20:49:34 +0200

    correction

--- src/README.md
+++
@@ -1,3 +0,0 @@
-Bibliothèque sous licence MIT
-
-Outils ajoutant quelques opérations bien utils sur certaines classes
--- src/druzy/version/Global.java
+++
@@ -1,9 +0,0 @@
-package druzy.version;
-
-public class Global {
-
-	public static final Version VERSION=new Version("1");
-	
-	private Global() {}
-
-}
